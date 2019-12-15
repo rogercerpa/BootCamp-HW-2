2b189312bc166d6963ecd80d60d80761d344def4
Roger Cerpa <roger.e.cerpa@gmail.com>
2019-12-15 17:38:24 -0500

    added variables and click button functions, basic info

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,10 @@
 var passwordgenerator = prompt('Would you like to generate a strong password?');
 var passwordlenght = prompt('how long would you like your password to be?');
+
+//button generate password
+
+function generatefunction() {}
+
+//button copy to clipboard
+
+function copytoclipboard() {}
