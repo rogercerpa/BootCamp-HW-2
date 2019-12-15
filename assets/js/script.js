88ceb667c3ef15bd36f5704ef0f2a757ee60307b
Roger Cerpa <roger.e.cerpa@gmail.com>
2019-12-15 17:55:15 -0500

    added initial conditions to the js file, welcome message

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,20 @@
-var passwordgenerator = prompt('Would you like to generate a strong password?');
-var passwordlenght = prompt('how long would you like your password to be?');
+alert('Hello! welcome to this Super cool password generator');
+
+var passwordgenerator = confirm(
+	'Would you like to generate a strong password?'
+);
+
+// var passwordlenght = prompt(
+// 	'how long characters you would like your password to be?'
+// );
+
+if (passwordgenerator) {
+	var passwordlenght = prompt(
+		'how long characters you would like your password to be?'
+	);
+} else {
+	alert('bye bye! have a nice day');
+}
 
 //button generate password
 
