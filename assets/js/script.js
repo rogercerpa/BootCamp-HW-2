5779ff805445591e32cad0079f99bc6b8ca921cb
Roger Cerpa <roger.e.cerpa@gmail.com>
2019-12-15 22:07:35 -0500

    added variables confirm and prompt asking for the desired password

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,17 +4,61 @@ var passwordgenerator = confirm(
 	'Would you like to generate a strong password?'
 );
 
-// var passwordlenght = prompt(
-// 	'how long characters you would like your password to be?'
-// );
-
 if (passwordgenerator) {
 	var passwordlenght = prompt(
-		'how long characters you would like your password to be?'
+		'How many characters would you like your password to have?'
+	);
+
+	var uppercaseinput = confirm(
+		'Would you like upper case letters included in your password?'
+	);
+
+	var lowercaseinput = confirm(
+		'Would you like lower case letters included in your password?'
+	);
+
+	var numberinput = confirm(
+		'Would you like numbers included in your password?'
+	);
+
+	var symbolsinput = confirm(
+		'would you like symbols included in your password?'
 	);
 } else {
 	alert('bye bye! have a nice day');
 }
+
+var uppercase = [
+	'A',
+	'B',
+	'C',
+	'D',
+	'E',
+	'F',
+	'G',
+	'H',
+	'I',
+	'J',
+	'K',
+	'L',
+	'M',
+	'N',
+	'O',
+	'P',
+	'Q',
+	'R',
+	'S',
+	'T',
+	'U',
+	'V',
+	'W',
+	'X',
+	'Y',
+	'Z'
+];
+var lowercase = uppercase.toLowercase;
+
+var numbers = [ '1', '2', '3', '4', '5', '6', '7', '8', '9', '0' ];
 
 //button generate password
 
