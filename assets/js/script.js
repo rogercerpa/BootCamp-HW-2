1526e3d52b693b5332c9698fc498dffe3a51931b
Roger Cerpa <roger.e.cerpa@gmail.com>
2019-12-17 19:16:05 -0500

    added a new image to the the image folder, updated the background image

--- assets/js/script.js
+++ assets/js/script.js
@@ -28,38 +28,38 @@ if (passwordgenerator) {
 	alert('bye bye! have a nice day');
 }
 
-var randomUppercase = [
-	'A',
-	'B',
-	'C',
-	'D',
-	'E',
-	'F',
-	'G',
-	'H',
-	'I',
-	'J',
-	'K',
-	'L',
-	'M',
-	'N',
-	'O',
-	'P',
-	'Q',
-	'R',
-	'S',
-	'T',
-	'U',
-	'V',
-	'W',
-	'X',
-	'Y',
-	'Z'
-];
+// var randomUppercase = [
+// 	'A',
+// 	'B',
+// 	'C',
+// 	'D',
+// 	'E',
+// 	'F',
+// 	'G',
+// 	'H',
+// 	'I',
+// 	'J',
+// 	'K',
+// 	'L',
+// 	'M',
+// 	'N',
+// 	'O',
+// 	'P',
+// 	'Q',
+// 	'R',
+// 	'S',
+// 	'T',
+// 	'U',
+// 	'V',
+// 	'W',
+// 	'X',
+// 	'Y',
+// 	'Z'
+// ];
 
 // var lowercase = uppercase.toLowerCase();
 
-var numbers = [ '1', '2', '3', '4', '5', '6', '7', '8', '9', '0' ];
+// var numbers = [ '1', '2', '3', '4', '5', '6', '7', '8', '9', '0' ];
 
 //button generate password
 
